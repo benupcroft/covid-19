b4de4bc1e4689c52a0b60a9883a45f479d655f3a
Ben Upcroft <ben@oxbotica.com>
2020-04-27 19:09:43 +0100

    modified jupyter files and load uk data

---
+++ rt_live_uk.py
@@ -0,0 +1,558 @@
+#!/usr/bin/python3.7
+# -*- coding: utf-8 -*-
+"""
+# Estimating COVID-19's $R_t$ in Real-Time - UK
+
+Ben and Issac Upcroft adapted from Kevin Systrom's notebook - April 22
+
+Model originally built by [Thomas Vladeck](https://github.com/tvladeck) in Stan, parts inspired by the work over at https://epiforecasts.io/, lots of help from [Thomas Wiecki](https://twitter.com/twiecki). Thank you to everyone who helped.
+"""
+
+# Commented out IPython magic to ensure Python compatibility.
+# For some reason Theano is unhappy when I run the GP, need to disable future warnings
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+import os
+import io
+import requests
+import pymc3 as pm
+import pandas as pd
+import numpy as np
+import theano
+import theano.tensor as tt
+
+from matplotlib import pyplot as plt
+from matplotlib import dates as mdates
+from matplotlib import ticker
+
+from datetime import date
+from datetime import datetime
+
+from IPython.display import clear_output
+
+import shutil
+import time
+
+
+# %config InlineBackend.figure_format = 'retina'
+
+"""## Load local authority information and convert to regions
+#### Load
+"""
+
+# proxies = {
+#  “http”: “http://10.10.10.10:8000”,
+#  “https”: “http://10.10.10.10:8000”,
+# }
+
+url = 'https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv'
+datastr = requests.get('https://coronavirus.data.gov.uk/downloads/csv/coronavirus-cases_latest.csv',
+                       allow_redirects=True).text
+data_file = io.StringIO(datastr)
+covid_cases = pd.read_csv(data_file,
+                          parse_dates=['Specimen date'],
+                          index_col=['Area name', 'Specimen date']).sort_index()
+
+regions = covid_cases[covid_cases['Area type'] == 'Region']
+
+#
+# mapped local authorities into regions
+#
+
+# url = 'https://opendata.arcgis.com/datasets/c457af6314f24b20bb5de8fe41e05898_0.csv'
+# local_to_region_lookup = pd.read_csv(url)
+# lookup_dict = pd.Series(local_to_region_lookup.RGN17NM.values,index=local_to_region_lookup.LAD17NM).to_dict()
+# covid_cases['Region'] = covid_cases['Area name'].map(lookup_dict)
+
+# display(regions)
+
+"""#### Integrity Check"""
+
+# # Make sure that all the states have current data
+today = datetime.combine(date.today(), datetime.min.time())
+last_updated = regions.reset_index('Specimen date').groupby('Area name')['Specimen date'].max()
+is_current = last_updated < today
+
+try:
+    assert is_current.sum() == 0
+except AssertionError:
+    print("Not all regions have updated")
+#    display(last_updated[is_current])
+
+# Ensure all case diffs are greater than zero
+for region, grp in regions.groupby('Area name'):
+    new_cases = grp['Cumulative lab-confirmed cases'].diff().dropna()
+    is_positive = new_cases.ge(0)
+
+    try:
+        assert is_positive.all()
+    except AssertionError:
+        print(f"Warning: {region} has date with negative case counts")
+#        display(new_cases[~is_positive])
+
+# display(regions)
+
+"""## Load Patient Information
+#### Download
+~100mb download (be ... patient!)
+"""
+
+def download_file(url, local_filename):
+    """From https://stackoverflow.com/questions/16694907/"""
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(local_filename, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+    return local_filename
+
+URL = "https://raw.githubusercontent.com/beoutbreakprepared/nCoV2019/master/latest_data/latestdata.csv"
+LINELIST_PATH = 'data/linelist.csv'
+
+print('Downloading file, this will take a while ~100mb')
+try:
+    download_file(URL, LINELIST_PATH)
+    clear_output(wait=True)
+    print('Done downloading.')
+except:
+    print('Something went wrong. Try again.')
+
+
+# if not os.path.exists(LINELIST_PATH):
+#     print('Downloading file, this will take a while ~100mb')
+#     try:
+#         download_file(URL, LINELIST_PATH)
+#         clear_output(wait=True)
+#         print('Done downloading.')
+#     except:
+#         print('Something went wrong. Try again.')
+# else:
+#     print('Already downloaded CSV')
+
+"""#### Parse & Clean Patient Info"""
+
+# Load the patient CSV
+patients = pd.read_csv(
+    'data/linelist.csv',
+    parse_dates=False,
+    usecols=[
+        'date_confirmation',
+        'date_onset_symptoms'],
+    low_memory=False)
+
+patients.columns = ['Onset', 'Confirmed']
+
+# There's an errant reversed date
+patients = patients.replace('01.31.2020', '31.01.2020')
+
+# Only keep if both values are present
+patients = patients.dropna()
+
+# Must have strings that look like individual dates
+# "2020.03.09" is 10 chars long
+is_ten_char = lambda x: x.str.len().eq(10)
+patients = patients[is_ten_char(patients.Confirmed) &
+                    is_ten_char(patients.Onset)]
+
+# Convert both to datetimes
+patients.Confirmed = pd.to_datetime(
+    patients.Confirmed, format='%d.%m.%Y')
+patients.Onset = pd.to_datetime(
+    patients.Onset, format='%d.%m.%Y')
+
+# Only keep records where confirmed > onset
+patients = patients[patients.Confirmed >= patients.Onset]
+
+"""#### Show Relationship between Onset of Symptoms and Confirmation"""
+
+ax = patients.plot.scatter(
+    title='Onset vs. Confirmed Dates - COVID19',
+    x='Onset',
+    y='Confirmed',
+    alpha=.1,
+    lw=0,
+    s=10,
+    figsize=(6,6))
+
+formatter = mdates.DateFormatter('%m/%d')
+locator = mdates.WeekdayLocator(interval=2)
+
+for axis in [ax.xaxis, ax.yaxis]:
+    axis.set_major_formatter(formatter)
+    axis.set_major_locator(locator)
+
+"""#### Calculate the Probability Distribution of Delay"""
+
+# Calculate the delta in days between onset and confirmation
+delay = (patients.Confirmed - patients.Onset).dt.days
+
+# Convert samples to an empirical distribution
+p_delay = delay.value_counts().sort_index()
+new_range = np.arange(0, p_delay.index.max()+1)
+p_delay = p_delay.reindex(new_range, fill_value=0)
+p_delay /= p_delay.sum()
+
+# Show our work
+fig, axes = plt.subplots(ncols=2, figsize=(9,3))
+p_delay.plot(title='P(Delay)', ax=axes[0])
+p_delay.cumsum().plot(title='P(Delay <= x)', ax=axes[1])
+for ax in axes:
+    ax.set_xlabel('days')
+
+"""## A Single Region - South East (contains Oxford)
+
+#### Select Region Data
+"""
+
+region = 'South East'
+confirmed = regions.xs(region)['Cumulative lab-confirmed cases'].diff().dropna()
+confirmed.tail()
+
+"""### Translate Confirmation Dates to Onset Dates
+
+Our goal is to translate positive test counts to the dates where they likely occured. Since we have the distribution, we can distribute case counts back in time according to that distribution. To accomplish this, we reverse the case time series, and convolve it using the distribution of delay from onset to confirmation. Then we reverse the series again to obtain the onset curve. Note that this means the data will be 'right censored' which means there are onset cases that have yet to be reported so it looks as if the count has gone down.
+"""
+
+def confirmed_to_onset(confirmed, p_delay):
+
+    assert not confirmed.isna().any()
+
+    # Reverse cases so that we convolve into the past
+    convolved = np.convolve(confirmed[::-1].values, p_delay)
+
+    # Calculate the new date range
+    dr = pd.date_range(end=confirmed.index[-1],
+                       periods=len(convolved))
+
+    # Flip the values and assign the date range
+    onset = pd.Series(np.flip(convolved), index=dr)
+
+    return onset
+
+
+onset = confirmed_to_onset(confirmed, p_delay)
+
+"""### Adjust for Right-Censoring
+
+Since we distributed observed cases into the past to recreate the onset curve, we now have a right-censored time series. We can correct for that by asking what % of people have a delay less than or equal to the time between the day in question and the current day.
+
+For example, 5 days ago, there might have been 100 cases onset. Over the course of the next 5 days some portion of those cases will be reported. This portion is equal to the cumulative distribution function of our delay distribution. If we know that portion is say, 60%, then our current count of onset on that day represents 60% of the total. This implies that the total is 166% higher. We apply this correction to get an idea of what actual onset cases are likely, thus removing the right censoring.
+"""
+
+def adjust_onset_for_right_censorship(onset, p_delay):
+    cumulative_p_delay = p_delay.cumsum()
+
+    # Calculate the additional ones needed so shapes match
+    ones_needed = len(onset) - len(cumulative_p_delay)
+    padding_shape = (0, ones_needed)
+
+    # Add ones and flip back
+    cumulative_p_delay = np.pad(
+        cumulative_p_delay,
+        padding_shape,
+        mode='constant',
+        constant_values=1)
+    cumulative_p_delay = np.flip(cumulative_p_delay)
+
+    # Adjusts observed onset values to expected terminal onset values
+    adjusted = onset / cumulative_p_delay
+
+    return adjusted, cumulative_p_delay
+
+
+adjusted, cumulative_p_delay = adjust_onset_for_right_censorship(onset, p_delay)
+
+"""Take a look at all three series: confirmed, onset and onset adjusted for right censoring."""
+
+fig, ax = plt.subplots(figsize=(5,3))
+
+confirmed.plot(
+    ax=ax,
+    label='Confirmed',
+    title=region,
+    c='k',
+    alpha=.25,
+    lw=1)
+
+onset.plot(
+    ax=ax,
+    label='Onset',
+    c='k',
+    lw=1)
+
+adjusted.plot(
+    ax=ax,
+    label='Adjusted Onset',
+    c='k',
+    linestyle='--',
+    lw=1)
+
+ax.legend();
+
+"""Let's have the model run on days where we have enough data ~last 50 or so
+
+### Sample the Posterior with PyMC3
+
+We assume a poisson likelihood function and feed it what we believe is the onset curve based on reported data. We model this onset curve based on the same math in the previous notebook:
+
+$$ I^\prime = Ie^{\gamma(R_t-1)} $$
+
+We define $\theta = \gamma(R_t-1)$ and model $ I^\prime = Ie^{\theta} $ where $\theta$ observes a random walk. We let $\gamma$ vary independently based on known parameters for the serial interval. Therefore, we can recover $R_t$ easily by $R_t = \frac{\theta}{\gamma}+1$
+
+The only tricky part is understanding that we're feeding in _onset_ cases to the likelihood. So $\mu$ of the poisson is the positive, non-zero, expected onset cases we think we'd see today.
+
+We calculate this by figuring out how many cases we'd expect there to be yesterday total when adjusted for bias and plugging it into the first equation above. We then have to re-bias this number back down to get the expected amount of onset cases observed that day.
+"""
+
+class MCMCModel(object):
+
+    def __init__(self, region, onset, cumulative_p_delay, window=50):
+
+        # Just for identification purposes
+        self.region = region
+
+        # For the model, we'll only look at the last N
+        self.onset = onset.iloc[-window:]
+        self.cumulative_p_delay = cumulative_p_delay[-window:]
+
+        # Where we store the results
+        self.trace = None
+        self.trace_index = self.onset.index[1:]
+
+    def run(self, chains=1, tune=3000, draws=1000, target_accept=.95):
+
+        with pm.Model() as model:
+
+            # Random walk magnitude
+            step_size = pm.HalfNormal('step_size', sigma=.03)
+
+            # Theta random walk
+            theta_raw_init = pm.Normal('theta_raw_init', 0.1, 0.1)
+            theta_raw_steps = pm.Normal('theta_raw_steps', shape=len(self.onset)-2) * step_size
+            theta_raw = tt.concatenate([[theta_raw_init], theta_raw_steps])
+            theta = pm.Deterministic('theta', theta_raw.cumsum())
+
+            # Let the serial interval be a random variable and calculate r_t
+            serial_interval = pm.Gamma('serial_interval', alpha=6, beta=1.5)
+            gamma = 1.0 / serial_interval
+            r_t = pm.Deterministic('r_t', theta/gamma + 1)
+
+            inferred_yesterday = self.onset.values[:-1] / self.cumulative_p_delay[:-1]
+
+            expected_today = inferred_yesterday * self.cumulative_p_delay[1:] * pm.math.exp(theta)
+
+            # Ensure cases stay above zero for poisson
+            mu = pm.math.maximum(.1, expected_today)
+            observed = self.onset.round().values[1:]
+            cases = pm.Poisson('cases', mu=mu, observed=observed)
+
+            self.trace = pm.sample(
+                chains=chains,
+                tune=tune,
+                draws=draws,
+                target_accept=target_accept)
+
+            return self
+
+    def run_gp(self):
+        with pm.Model() as model:
+            gp_shape = len(self.onset) - 1
+
+            length_scale = pm.Gamma("length_scale", alpha=3, beta=.4)
+
+            eta = .05
+            cov_func = eta**2 * pm.gp.cov.ExpQuad(1, length_scale)
+
+            gp = pm.gp.Latent(mean_func=pm.gp.mean.Constant(c=0),
+                              cov_func=cov_func)
+
+            # Place a GP prior over the function f.
+            theta = gp.prior("theta", X=np.arange(gp_shape)[:, None])
+
+            # Let the serial interval be a random variable and calculate r_t
+            serial_interval = pm.Gamma('serial_interval', alpha=6, beta=1.5)
+            gamma = 1.0 / serial_interval
+            r_t = pm.Deterministic('r_t', theta / gamma + 1)
+
+            inferred_yesterday = self.onset.values[:-1] / self.cumulative_p_delay[:-1]
+            expected_today = inferred_yesterday * self.cumulative_p_delay[1:] * pm.math.exp(theta)
+
+            # Ensure cases stay above zero for poisson
+            mu = pm.math.maximum(.1, expected_today)
+            observed = self.onset.round().values[1:]
+            cases = pm.Poisson('cases', mu=mu, observed=observed)
+
+            self.trace = pm.sample(chains=1, tune=1000, draws=1000, target_accept=.8)
+        return self
+
+"""### Run Pymc3 Model"""
+
+def df_from_model(model):
+
+    r_t = model.trace['r_t']
+    mean = np.mean(r_t, axis=0)
+    median = np.median(r_t, axis=0)
+    hpd_90 = pm.stats.hpd(r_t, .9)
+    hpd_50 = pm.stats.hpd(r_t, .5)
+    # hpd_90 = pm.stats.hpd(r_t)
+    # hpd_50 = pm.stats.hpd(r_t)
+
+    idx = pd.MultiIndex.from_product([
+            [model.region],
+            model.trace_index
+        ], names=['region', 'date'])
+
+    df = pd.DataFrame(data=np.c_[mean, median, hpd_90, hpd_50], index=idx,
+                 columns=['mean', 'median', 'lower_90', 'upper_90', 'lower_50','upper_50'])
+    return df
+
+def create_and_run_model(name, region):
+    confirmed = region['Cumulative lab-confirmed cases'].diff().dropna()
+    onset = confirmed_to_onset(confirmed, p_delay)
+    adjusted, cumulative_p_delay = adjust_onset_for_right_censorship(onset, p_delay)
+    return MCMCModel(name, onset, cumulative_p_delay).run()
+
+models = {}
+
+for region, grp in regions.groupby('Area name'):
+
+    print(region)
+
+    if region in models:
+        print(f'Skipping {region}, already in cache')
+        continue
+
+    models[region] = create_and_run_model(region,grp.droplevel(0))
+
+"""### Handle Divergences"""
+
+# Check to see if there were divergences
+n_diverging = lambda x: x.trace['diverging'].nonzero()[0].size
+divergences = pd.Series([n_diverging(m) for m in models.values()], index=models.keys())
+has_divergences = divergences.gt(0)
+
+print('Diverging states:')
+# display(divergences[has_divergences])
+
+# Rerun states with divergences
+for region, n_divergences in divergences[has_divergences].items():
+    models[region].run()
+
+"""## Compile Results"""
+
+results = None
+
+for region, model in models.items():
+
+    df = df_from_model(model)
+
+    if results is None:
+        results = df
+    else:
+        results = pd.concat([results, df], axis=0)
+
+"""### Render to CSV
+Uncomment if you'd like
+"""
+
+# results.to_csv('data/rt_2020_04_26.csv')
+
+"""### Render Charts"""
+
+def plot_rt(name, result, ax, c=(.3,.3,.3,1), ci=(0,0,0,.05)):
+    ax.set_ylim(0.5, 1.6)
+    ax.set_title(name)
+    ax.plot(result['median'],
+            marker='o',
+            markersize=4,
+            markerfacecolor='w',
+            lw=1,
+            c=c,
+            markevery=2)
+    ax.fill_between(
+        result.index,
+        result['lower_90'].values,
+        result['upper_90'].values,
+        color=ci,
+        lw=0)
+    ax.axhline(1.0, linestyle=':', lw=1)
+
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%d/%m'))
+    ax.xaxis.set_major_locator(mdates.WeekdayLocator(interval=2))
+
+ncols = 3
+nrows = int(np.ceil(results.index.levels[0].shape[0] / ncols))
+
+fig, axes = plt.subplots(
+    nrows=nrows,
+    ncols=ncols,
+    figsize=(14, nrows*3),
+    sharey='row')
+
+# display(results)
+
+for ax, (region, result) in zip(axes.flat, results.groupby('region')):
+    plot_rt(region, result.droplevel(0), ax)
+
+fig.tight_layout()
+fig.set_facecolor('w')
+
+# move the old plot and save
+timestr = time.strftime("%Y%m%d-%H%M%S")
+# print timestr
+shutil.move("../../mysite/static/img/latest.png", "../../mysite/static/img/old_plots/rt_plot-"+timestr+".png")
+
+fig.savefig('../../mysite/static/img/latest.png', dpi=fig.dpi)
+
+
+# plt.show()
+
+
+
+############
+# url = 'https://raw.githubusercontent.com/jasonong/List-of-US-States/master/states.csv'
+# abbrev = pd.read_csv(url, index_col=['State'], squeeze=True)
+
+# jhu_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_US.csv'
+# jhu = pd.read_csv(jhu_url)
+
+# jhu = jhu.drop(columns=['UID', 'iso2', 'iso3', 'code3', 'FIPS', 'Admin2', 'Country_Region', 'Lat', 'Long_', 'Combined_Key'])
+# jhu = jhu.set_index('Province_State')
+# jhu.index = jhu.index.rename('region')
+# jhu = jhu.drop([
+#     'American Samoa',
+#     'Guam',
+#     'Northern Mariana Islands',
+#     'Puerto Rico',
+#     'Virgin Islands',
+#     'Diamond Princess',
+#     'Grand Princess'])
+
+# jhu.index = pd.Index(jhu.index.to_series().replace(abbrev).values, name='region')
+# jhu.columns = pd.to_datetime(jhu.columns)
+# jhu = jhu.groupby('region').sum()
+# jhu = jhu.stack().sort_index()
+
+# state = 'VT'
+# ax = jhu.xs(state).diff().plot(label='JHU', color='k', legend=True, title=state)
+# # jhu.xs('AK').diff().rolling(7).mean().plot(ax=ax)
+# states.xs(state).positive.diff().plot(ax=ax, figsize=(6,4), linestyle=':', label='Covidtracking', legend=True)
+# ax.set_xlim(pd.Timestamp('2020-03-01'),None)
+
+# url = 'https://raw.githubusercontent.com/epiforecasts/covid-regional/3ad63ea1acceb797f0628a8037fc206342d267e7/united-states/regional-summary/rt.csv'
+
+# epf = pd.read_csv(url, parse_dates=['date'])
+# epf.region = epf.region.replace(abbrev)
+# epf = epf.set_index(['region', 'date']).sort_index()
+# epf = epf.drop(['Guam', 'Puerto Rico'])
+
+# epf_lookup = {}
+# for idx, grp in epf.groupby('region'):
+
+#     epf_lookup[idx]=grp
+#     epf_grp = epf_lookup[state]
+
+#     plot_rt(state, result.droplevel(0), ax)
+#     plot_rt(state, epf_grp.droplevel(0), ax, c=(1,0,0,1))
